10c0f5c533a119501542a539deacc840583bec28
shiva <shivain22@gmail.com>
2023-01-06 10:12:37 +0530

    Ajax training session.

--- wicket-training/src/main/java/com/gel/wicket_training/IndexPage.java
+++ wicket-training/src/main/java/com/gel/wicket_training/IndexPage.java
@@ -1,4 +1,6 @@
 package com.gel.wicket_training;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
@@ -7,7 +9,16 @@ public  class IndexPage extends WebPage {
 	public IndexPage() {
 		add(new BookmarkablePageLink("basicCrudPage", BasicCrudPage.class));
 		add(new BookmarkablePageLink("ajaxPage", AjaxPage.class));
+		add(new BookmarkablePageLink("ajaxFormInTabbedPanel", AjaxFormInTabbedPanel.class));
 		add(new Label("footer", "This is the footer"));
 		add(new Label("hello","Apache Wicket Training"));
+	}
+	
+	@Override
+	public void renderHead(IHeaderResponse response) {
+	    super.renderHead(response);
+
+	    response.render(JavaScriptHeaderItem.forReference(getApplication().getJavaScriptLibrarySettings()
+	        .getJQueryReference()));  
 	}
 }
